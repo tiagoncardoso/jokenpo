da735c9fe9b9f24501853ebbb261687e8e7dbbef
Tiago Cardoso <tiago.mncardoso@gmail.com>
2019-12-02 18:52:54 -0300

    Finalizando lógica

--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -1,13 +1,117 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Jogo extends StatefulWidget {
   @override
   _JogoState createState() => _JogoState();
 }
 
+class Bot {
+  var _usrOpt;
+  var _botOpt;
+  var _winner;
+
+  Bot(int option) {
+    this._usrOpt = option;
+    this._botOpt = new Random().nextInt(3);
+    this._winner = this.winner();
+  }
+
+  int winner() {
+    // _player 1 : empate
+    // _player 2: usuário
+    // _player 3: máquina
+    int _player;
+
+    if (this.botOpt == 0) { // Máquina colocou pedra
+      switch(this.usrOpt) {
+        case 0: // Usuário colocou pedra
+          _player = 1; //empate
+          break;
+        case 1: // Usuário colocou papel
+          _player = 2; // usuário ganhou
+          break;
+        case 2: // Usuário colocou tesoura
+          _player = 3; // máquina ganhou
+          break;
+      }
+    } else if (this.botOpt == 1) { // Máquina colocou papel
+      switch(this.usrOpt) {
+        case 0: // Usuário colocou pedra
+          _player = 3; // máquina ganhou
+          break;
+        case 1: // Usuário colocou papel
+          _player = 1; // empate
+          break;
+        case 2: // Usuário colocou tesoura
+          _player = 2; // usuário ganhou
+          break;
+      }
+    } else { // Máquina colocou tesoura
+      switch(this.usrOpt) {
+        case 0: // Usuário colocou pedra
+          _player = 2; // usuário ganhou
+          break;
+        case 1: // Usuário colocou papel
+          _player = 3; // máquina ganhou
+          break;
+        case 2: // Usuário colocou tesoura
+          _player = 1; // empate
+          break;
+      }
+    }
+
+    return _player;
+  }
+
+  String result() {
+    String _res;
+
+    switch(this.win) {
+      case 1:
+        _res = 'Jogo empatou! Tente outra vez!';
+        break;
+      case 2:
+        _res = 'Parabéns! Você venceu!!! :-)';
+        break;
+      case 3:
+        _res = 'Dessa vez não deu! Continue tentando! :-(';
+        break;
+    }
+
+    return _res;
+  }
+
+  int get win {
+    return this._winner;
+  }
+
+  int get botOpt {
+    return this._botOpt;
+  }
+
+  int get usrOpt {
+    return this._usrOpt;
+  }
+
+}
+
 class _JogoState extends State<Jogo> {
+  var _imagensApp;
+  var _idx = 0;
+  var jogar;
+  var _result = 'Clique em uma das opções abaixo:';
+
   @override
   Widget build(BuildContext context) {
+
+    this._imagensApp = [
+      AssetImage('images/padrao.png'),
+      AssetImage('images/pedra.png'),
+      AssetImage('images/papel.png'),
+      AssetImage('images/tesoura.png')
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -26,7 +130,7 @@ class _JogoState extends State<Jogo> {
           )
         ),
         width: double.infinity,
-        padding: EdgeInsets.all(30),
+        padding: EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -35,22 +139,23 @@ class _JogoState extends State<Jogo> {
              Text(
                   'Escolha do App',
                   style: TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold
+                      fontSize: 25,
+                      fontFamily: 'Sansation',
+                      fontWeight: FontWeight.w700
                   ),
                textAlign: TextAlign.center,
               ),
-             Image.asset(
-                 'images/pedra.png'
+             Image(
+               image: this._imagensApp[this._idx],
+               height: 250,
              ),
              Padding(
-               padding: EdgeInsets.all(60),
+               padding: EdgeInsets.all(30),
                child: Text(
-                 'Você perdeu :-(',
+                 this._result,
                  style: TextStyle(
                    fontSize: 30,
-                   fontFamily: 'Sansation',
-                   fontWeight: FontWeight.w700
+                   fontFamily: 'Sansation'
                  ),
                  textAlign: TextAlign.center,
                ),
@@ -60,38 +165,48 @@ class _JogoState extends State<Jogo> {
                child: Row(
                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                  children: <Widget>[
-                   OutlineButton(
-                       onPressed: null,
-                       borderSide: BorderSide(
-                         color: Colors.red,
-                         style: BorderStyle.solid,
-                         width: 3
-                       ),
-                       shape: CircleBorder(),
-                       padding: EdgeInsets.all(15),
-                       child: Image.asset('images/pedra.png')
+                   GestureDetector(
+                     onTap: () {
+                       setState(() {
+                         jogar = new Bot(0);
+                         this._idx = jogar.botOpt + 1;
+                         this._result = jogar.result();
+                       });
+
+                     },
+
+                     child: Image.asset(
+                       'images/pedra.png',
+                       height: 100,
+                     ),
                    ),
-                   OutlineButton(
-                       onPressed: null,
-                       borderSide: BorderSide(
-                         style: BorderStyle.solid,
-                           width: 3,
-                           color: Colors.red
-                       ),
-                       shape: CircleBorder(),
-                       padding: EdgeInsets.all(15),
-                       child: Image.asset('images/papel.png')
+                   GestureDetector(
+                     onTap: (){
+                       setState(() {
+                         jogar = new Bot(1);
+                         this._idx = jogar.botOpt + 1;
+                         this._result = jogar.result();
+                       });
+                     },
+
+                     child: Image.asset(
+                       'images/papel.png',
+                       height: 100,
+                     ),
                    ),
-                   OutlineButton(
-                       onPressed: null,
-                       borderSide: BorderSide(
-                         color: Colors.red,
-                         style: BorderStyle.solid,
-                         width: 3
-                       ),
-                       shape: CircleBorder(),
-                       padding: EdgeInsets.all(15),
-                       child: Image.asset('images/tesoura.png')
+                   GestureDetector(
+                     onTap: () {
+                       setState(() {
+                         jogar = new Bot(2);
+                         this._idx = jogar.botOpt + 1;
+                         this._result = jogar.result();
+                       });
+                     },
+
+                     child: Image.asset(
+                       'images/tesoura.png',
+                       height: 100,
+                     ),
                    ),
                  ],
                ),
